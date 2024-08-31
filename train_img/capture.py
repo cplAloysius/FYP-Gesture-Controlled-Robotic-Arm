0d1adaf40d02a08bac8a8ff60aa02a8775d39b7e
Aloysius Loh <diamondhead85@gmail.com>
2024-08-31 15:09:59 +0800

    add training images

---
+++ train_img/capture.py
@@ -0,0 +1,20 @@
+import cv2
+import time
+count = 0
+maxFrames = 10
+
+cap=cv2.VideoCapture(0)
+
+while count < maxFrames:
+    ret, img = cap.read()
+    img = cv2.resize(img, (640,480))
+    img2 = img.copy()
+    img2 = cv2.putText(img2, str(count), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
+    cv2.imshow("test window", img2)
+    if cv2.waitKey(1) & 0xFF == ord(' '):
+    	cv2.imwrite("/home/pi/train_img/img/stickers3_%d.jpg" %count, img)
+    	count += 1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
